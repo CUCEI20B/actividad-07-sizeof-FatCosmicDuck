90e78c75d8f32124b977475a1015db88cb78801c
Karime Daniela Perez Juarez <karime.perez5588@alumnos.udg.mx>
2020-09-28 13:21:55 -0500

    Validacion de cadenas y uso del sizeof para variables

--- main.cpp
+++ main.cpp
@@ -3,7 +3,29 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    
+    string dato;
+
+    cin >> dato;
+
+    if(dato.compare("char")==0){
+        cout << sizeof(char) << endl;
+    }
+    else if(dato.compare("int")==0){
+        cout << sizeof(int) << endl;
+    }
+    else if(dato.compare("short")==0){
+        cout << sizeof(short) << endl;
+    }
+    else if(dato.compare("float")==0){
+        cout << sizeof(float) << endl;
+    }
+    else if(dato.compare("double")==0){
+        cout << sizeof(double) << endl;
+    }
+    else if(dato.compare("long")==0){
+        cout << sizeof(long) << endl;
+    }
 
     return 0;
 }
